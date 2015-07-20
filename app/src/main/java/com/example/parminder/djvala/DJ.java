4d9b09bf272cfb9c87efd82f0332460a6112d3d1
Jatin Kinra <jatin.kinra95@gmail.com>
2015-07-20 07:05:52 -0700

    Completed

---
+++ app/src/main/java/com/example/parminder/djvala/DJ.java
@@ -0,0 +1,48 @@
+package com.example.parminder.djvala;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class DJ extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int secondsDelayed = 3;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), Music.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
